55f2aa38402ccf395c9a46516e06f55c4161a0ff
Oli Wennell <oli.wennell@gmail.com>
2013-12-14 22:59:17 +0000

    Added namespacing

--- specs/GameOfLifeSpec.js
+++ specs/GameOfLifeSpec.js
@@ -4,56 +4,61 @@ describe("A live cell", function () {
     describe("dies", function () {
 
         it("when it has no live neighbours", function () {
-            var nextGeneration = GameOfLife.step([
-                0, 0, 0,
-                0, 1, 0,
-                0, 0, 0
-            ]);
+            var nextGeneration = GameOfLife.Simulation.step(
+                GameOfLife.GridCreator.createFromCells([
+                    0, 0, 0,
+                    0, 1, 0,
+                    0, 0, 0
+                ]));
 
-            expect(nextGeneration[4]).toBe(0);
+            expect(nextGeneration.cells[4]).toBe(0);
         });
 
         it("when it has one live neighbour", function () {
-            var nextGeneration = GameOfLife.step([
-                0, 0, 0,
-                0, 1, 1,
-                0, 0, 0
-            ]);
+            var nextGeneration = GameOfLife.Simulation.step(
+                GameOfLife.GridCreator.createFromCells([
+                    0, 0, 0,
+                    0, 1, 1,
+                    0, 0, 0
+                ]));
 
-            expect(nextGeneration[4]).toBe(0);
+            expect(nextGeneration.cells[4]).toBe(0);
         });
 
         it("when it has more than three live neighbours", function () {
-            var nextGeneration = GameOfLife.step([
-                1, 1, 1,
-                1, 1, 0,
-                0, 0, 0
-            ]);
+            var nextGeneration = GameOfLife.Simulation.step(
+                GameOfLife.GridCreator.createFromCells([
+                    1, 1, 1,
+                    1, 1, 0,
+                    0, 0, 0
+                ]));
 
-            expect(nextGeneration[4]).toBe(0);
+            expect(nextGeneration.cells[4]).toBe(0);
         });
     });
 
     describe("stays alive", function () {
 
         it("when it has two live neighbours", function () {
-            var nextGeneration = GameOfLife.step([
-                0, 0, 0,
-                1, 1, 1,
-                0, 0, 0
-            ]);
+            var nextGeneration = GameOfLife.Simulation.step(
+                GameOfLife.GridCreator.createFromCells([
+                    0, 0, 0,
+                    1, 1, 1,
+                    0, 0, 0
+                ]));
 
-            expect(nextGeneration[4]).toBe(1);
+            expect(nextGeneration.cells[4]).toBe(1);
         });
 
         it("when it has three live neighbours", function () {
-            var nextGeneration = GameOfLife.step([
-                1, 0, 0,
-                1, 1, 1,
-                0, 0, 0
-            ]);
+            var nextGeneration = GameOfLife.Simulation.step(
+                GameOfLife.GridCreator.createFromCells([
+                    1, 0, 0,
+                    1, 1, 1,
+                    0, 0, 0
+                ]));
 
-            expect(nextGeneration[4]).toBe(1);
+            expect(nextGeneration.cells[4]).toBe(1);
         });
     });
 });
@@ -63,13 +68,14 @@ describe("A dead cell", function () {
     describe("comes alive", function () {
 
         it("when it has three live neighbours", function () {
-            var nextGeneration = GameOfLife.step([
-                1, 0, 0,
-                1, 0, 1,
-                0, 0, 0
-            ]);
+            var nextGeneration = GameOfLife.Simulation.step(
+                GameOfLife.GridCreator.createFromCells([
+                    1, 0, 0,
+                    1, 0, 1,
+                    0, 0, 0
+                ]));
 
-            expect(nextGeneration[4]).toBe(1);
+            expect(nextGeneration.cells[4]).toBe(1);
         });
     });
 });
